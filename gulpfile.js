b919b4725b3c3373cb52d39c007d0377c745f95a
noxan <noxan@byteweaver.net>
2015-01-24 01:25:26 +0100

    add scripts target with browserify to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
+
+gulp.task('scripts', function() {
+  var bundler = browserify('./src/app.js');
+  return bundler.bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./dist'));
+});
 
 gulp.task('default', function() {
   console.log('welcome to beer chronicles!');
