d7970db06e5200a002896c11a2cfcb3bb8ae1b1d
noxan <noxan@byteweaver.net>
2015-01-24 09:53:43 +0100

    add last night prototype of scene data with minimal display logic

--- src/app.js
+++ src/app.js
@@ -2,3 +2,25 @@ var angular = require('angular');
 
 
 var app = angular.module('beerChronicles', []);
+
+
+app.controller('GameCtrl', ['$scope', function($scope) {
+  $scope.scene = {
+    "id": 0,
+    "location": {
+      "backgroundImage": "assets/locations/tankstelle.jpg"
+    },
+    "choices": [
+      {
+        "text": "Wir gehen Bier holen an der Tanke!",
+        "href": 1
+      }, {
+        "text": "Wir warten hier erstmal.",
+        "href": 0
+      }, {
+        "text": "Wir suchen eine Brauerei.",
+        "href": 2
+      }
+    ]
+  };
+}]);
