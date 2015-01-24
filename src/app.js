177e74850f6d1ee8c35421760e57188609c49062
akuryou <akuryou@byteweaver.net>
2015-01-24 10:40:08 +0100

    load scenes from file

--- src/app.js
+++ src/app.js
@@ -4,23 +4,11 @@ var angular = require('angular');
 var app = angular.module('beerChronicles', []);
 
 
-app.controller('GameCtrl', ['$scope', function($scope) {
-  $scope.scene = {
-    "id": 0,
-    "location": {
-      "backgroundImage": "assets/locations/tankstelle.jpg"
-    },
-    "choices": [
-      {
-        "text": "Wir gehen Bier holen an der Tanke!",
-        "href": 1
-      }, {
-        "text": "Wir warten hier erstmal.",
-        "href": 0
-      }, {
-        "text": "Wir suchen eine Brauerei.",
-        "href": 2
-      }
-    ]
+app.controller('GameCtrl', ['$http', '$scope', function($http, $scope) {
+  var scenes = [];
+  $http.get("/assets/scenes.json").success(function(data) {
+    scenes = data;
+    $scope.scene = scenes[0];
+  });
   };
 }]);
