e347453241f4584447ab9786ad7e9791ad8b4ba0
axoila <axoila@gmx.de>
2015-01-24 22:16:30 +0100

    the brewery now REALLY is a funfair

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.controller('GameCtrl', ['$http', '$scope', '$log', '$q', function($http, $sc
 
   var locationsLoader = $http.get("/assets/locations.json");
   var charactersLoader = $http.get("/assets/characters.json");
-  var sceneLoaders = ["start", "apartment", "brewery", "gasStation", "lockUp"]
+  var sceneLoaders = ["start", "apartment", "gasStation", "lockUp", "funfair"]
       .map(function(resource) {
         return $http.get("/assets/scenes/" + resource + ".json");
       });
