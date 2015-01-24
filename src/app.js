dbc2269abf3ae1d5c796cc3b30690a8786b88ba3
Dennis Waldherr <therealdante@gmail.com>
2015-01-24 14:49:52 +0100

    Added setting/unsetting and depending on flags

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ var app = angular.module('beerChronicles', []);
 
 app.controller('GameCtrl', ['$http', '$scope', '$log', function($http, $scope, $log) {
   var scenes = [];
+  var globalState = {};
 
   var verifyScenes = function() {
     scenes.forEach(function(scene) {
@@ -25,11 +26,59 @@ app.controller('GameCtrl', ['$http', '$scope', '$log', function($http, $scope, $
   };
   $http.get("/assets/scenes.json").success(function(data) {
     scenes = data;
-    $scope.scene = scenes[0];
     verifyScenes();
+    $scope.gotoScene(0);
   });
 
+  var areConditionsFulfilled = function(condition, state) {
+    if (typeof condition == 'undefined')
+      return true;
+    var deniedBy = condition.deniedBy || [];
+    var result = true;
+    result = result && deniedBy.every(function(localFlag) {
+      return !(state[localFlag] || false); 
+    });
+    var requires = condition.requires || [];
+    result = result && requires.every(function(localFlag) {
+      return (state[localFlag] || false);
+    });
+    return result;
+  };
+
+  var applyStateChanges = function(changes, state) {
+    if (typeof changes == 'undefined')
+      return;
+    var flagsToSet = changes.set || [];
+    flagsToSet.forEach(function(localFlag) {
+      state[localFlag] = true;
+    });
+    var flagsToRemove= changes.unset || [];
+    flagsToRemove.forEach(function(localFlag) {
+      delete state[localFlag];
+    });
+  };
+
+  var determineChoices = function() {
+    var scene = $scope.scene;
+    var state = scene.state;
+    scene.choices.forEach(function(choice) {
+      choice.show = areConditionsFulfilled(choice.local, state)
+        && areConditionsFulfilled(choice.global, globalState);
+    });
+  };
+
+  $scope.onChoiceSelected = function(choice) {
+    var sceneState = $scope.scene.state;
+    applyStateChanges(choice.local, sceneState);
+    applyStateChanges(choice.global, globalState);
+    var id = choice.href;
+    $scope.gotoScene(id);
+  };
+
   $scope.gotoScene = function(id) {
-    $scope.scene = scenes[id];
+    var scene = scenes[id];
+    scene.state = scene.state || {};
+    $scope.scene = scene;
+    determineChoices();
   };
 }]);
